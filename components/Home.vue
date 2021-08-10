8418821044b960496909efa2f7f3d971d3a8a0d5
DefectingCat <i@defect.ink>
2021-08-10 11:00:25 +0800

    pages: init layout

---
+++ components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen">
+    <Menu />
+    <div class="flex items-center home-body">
+      <h1 class="text-7xl font-bold">Hi</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+export default defineComponent({
+  name: 'Home',
+  setup() {},
+});
+</script>
+
+<style scoped lang="scss">
+.home-body {
+  height: calc(100vh - (3rem + 50px));
+}
+</style>
---
+++ components/Menu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      class="
+        w-full
+        mt-8
+        mb-4
+        xl:mx-22rem
+        md:mx-10rem
+        sm:mx-8
+        mx-4
+        flex
+        justify-between
+        items-center
+      "
+    >
+      <img src="../static/tx-1.png" alt="" class="w-50px" />
+      <div class="flex justify-evenly">
+        <a
+          v-for="item of menus"
+          :key="item.id"
+          :href="item.href"
+          target="_blank"
+          class="sm:mx-4 mx-2"
+          >{{ item.name }}</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@nuxtjs/composition-api';
+export default defineComponent({
+  name: 'Menu',
+  setup() {
+    const menus = reactive([
+      {
+        id: 0,
+        href: 'https://www.defectink.com',
+        name: 'Blog',
+      },
+      {
+        id: 1,
+        href: 'https://www.github.com',
+        name: 'Github',
+      },
+      {
+        id: 2,
+        href: 'https://www.github.com',
+        name: 'Github',
+      },
+    ]);
+
+    return {
+      menus,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
