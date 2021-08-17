abf81b3d976a2d9d3488ac0bb7e53b5975417eae
DefectingCat <i@defect.ink>
2021-08-17 20:53:33 +0800

    add catch me component * add logos

---
+++ assets/catchMeInfo.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Telegram',
+    url: 'https://t.me/Defectink',
+    logo: 'telegram',
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/Defect___',
+    logo: 'twitter',
+  },
+  {
+    name: 'Mail',
+    url: 'https://gist.github.com/DefectingCat/d76be64d0da465e026eb7f17e126df39',
+    logo: 'outlook',
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com/DefectingCat',
+    logo: 'github',
+  },
+  {
+    name: 'Discord',
+    url: '',
+    logo: 'discord',
+  },
+  {
+    name: 'Steam',
+    url: 'https://steamcommunity.com/id/Defectink/',
+    logo: 'steam',
+  },
+  {
+    name: 'Skype',
+    url: '',
+    logo: 'skype',
+  },
+  {
+    name: 'Gitea',
+    url: 'https://git.defectink.com/xfy',
+    logo: 'gitea',
+  },
+];
--- components/CatchMe.vue
+++ components/CatchMe.vue
@@ -1,25 +1,60 @@
 <template>
   <div class="catch-me pb-8">
-    <CardTitle title="Catch Me" />
-    test
+    <CardTitle title="Catch me" sub-title="in somewhere" />
+
+    <div class="grid grid-cols-12 gap-x-8 p-4 <sm:gap-x-0">
+      <div class="col-span-2 col-start-1 <sm:hidden"></div>
+      <div
+        class="
+          col-span-8
+          flex
+          items-center
+          justify-evenly
+          <sm:col-span-12 <sm:flex-wrap
+        "
+      >
+        <div
+          v-for="item of info"
+          :key="item.name"
+          class="img-wrapper <sm:w-[25%] <sm:pb-4 cursor-pointer"
+        >
+          <a :href="item.url" target="_blank">
+            <img :src="require(`@/static/logos/${item.logo}.svg`)" alt="" />
+          </a>
+        </div>
+      </div>
+      <div class="col-span-2 col-start-1 <sm:hidden"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
 import CardTitle from './common/CardTitle.vue';
+import info from '@/assets/catchMeInfo';
 
 export default defineComponent({
   name: 'CatchMe',
   components: {
     CardTitle,
   },
-  setup() {},
+  setup() {
+    return {
+      info,
+    };
+  },
 });
 </script>
 
 <style scoped lang="scss">
 .catch-me {
   background-color: #f5f7f9;
+  & .img-wrapper {
+    transition: all 0.3s ease;
+  }
+  & .img-wrapper:hover {
+    transition: all 0.3s ease;
+    transform: scale(1.5);
+  }
 }
 </style>
